8d42a713ff53bfd7890942c6ba5c09a6f57f7520
Satya Siva Valluri <satya.maruth@gmail.com>
2020-03-10 14:08:46 -0500

    Create daysBetweenDates.py

---
+++ daysBetweenDates.py
@@ -0,0 +1,49 @@
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    False
+
+def daysInMonth(year, month):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
+
+def nextDay(year, month, day):
+    if day < 30:
+        return year, month, day + 1
+    else:
+        if month < 12:
+            return year, month + 1, 1
+        else:
+            return year + 1, 1, 1
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    False
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+
+print daysBetweenDates(2016, 2, 28, 2017, 2, 28)
